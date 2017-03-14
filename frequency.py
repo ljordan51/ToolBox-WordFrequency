75258517f78a9736877e92a543dff89056377fbc
ljordan51 <ljordan51@gmail.com>
2017-03-13 22:24:44 -0400

    turning in word frequency toolbox

--- frequency.py
+++ frequency.py
@@ -10,7 +10,18 @@ def get_word_list(file_name):
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
+    f = open(file_name, 'r')
+    lines = f.readlines()
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip(string.whitespace)
+    lines = ' '.join(lines)
+    lines = lines[lines.find('THE ADVENTURES  OF  HUCKLEBERRY FINN  (T'):]
+    words = lines.split()
+    for i in range(len(words)):
+        words[i] = words[i].lower()
+        words[i] = words[i].strip(string.punctuation)
+        words[i] = words[i].replace("'s", '')
+    return words
 
 
 def get_top_n_words(word_list, n):
@@ -23,8 +34,14 @@ def get_top_n_words(word_list, n):
     returns: a list of n most frequently occurring words ordered from most
     frequently to least frequentlyoccurring
     """
-    pass
+    word_dict = {}
+    for word in word_list:
+        word_dict[word] = word_dict.get(word, 0) + 1
+    ordered_by_frequency = sorted(word_dict, key=word_dict.get, reverse=True)
+    return ordered_by_frequency[:n]
+
 
 if __name__ == "__main__":
     print("Running WordFrequency Toolbox")
-    print(string.punctuation)
+    word_list = get_word_list('pg32325.txt')
+    print(get_top_n_words(word_list, 100))
